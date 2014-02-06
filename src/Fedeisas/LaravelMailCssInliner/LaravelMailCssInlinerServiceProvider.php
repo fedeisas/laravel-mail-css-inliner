43e7cc38eb984324c633464cf713c7204121ccd0
Fede Isas <fedeisas@hotmail.com>
2014-02-06 01:54:00 -0300

    initial commit

---
+++ src/Fedeisas/LaravelMailCssInliner/LaravelMailCssInlinerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Fedeisas\LaravelMailCssInliner;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMailCssInlinerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
