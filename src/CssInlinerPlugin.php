f1bd2670d18902ce8f40b1de0d275b406e8549ab
Roywcm <info@roywulms.nl>
2015-03-25 23:51:44 +0100

    Including external stylesheet
    Including external stylesheet

--- src/CssInlinerPlugin.php
+++ src/CssInlinerPlugin.php
@@ -6,13 +6,14 @@ use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
 
 class CssInlinerPlugin implements \Swift_Events_SendListener
 {
+
     /**
      * @param Swift_Events_SendEvent $evt
      */
     public function beforeSendPerformed(\Swift_Events_SendEvent $evt)
     {
         $message = $evt->getMessage();
-
+        
         $converter = new CssToInlineStyles();
         $converter->setEncoding($message->getCharset());
         $converter->setUseInlineStylesBlock();
@@ -21,8 +22,11 @@ class CssInlinerPlugin implements \Swift_Events_SendListener
         if ($message->getContentType() === 'text/html' ||
             ($message->getContentType() === 'multipart/alternative' && $message->getBody())
         ) {
+            $message->setBody($this->includeExternalStylesheets($message->getBody()),'text/html');
             $converter->setHTML($message->getBody());
             $message->setBody($converter->convert());
+            
+        
         }
 
         foreach ($message->getChildren() as $part) {
@@ -32,6 +36,48 @@ class CssInlinerPlugin implements \Swift_Events_SendListener
             }
         }
     }
+
+    public function includeExternalStylesheets($message)
+    {
+        preg_match_all('/<link[^>]+>/mi', $message, $matches);
+        $linkElements = $matches[0];
+        $count = count($linkElements);
+
+        if ($linkElements) {
+            foreach ($linkElements as $i => $linkElement) {
+                $cssContent = $this->getCssContent($linkElement);
+                $message = str_replace($linkElement, '
+                    <style type="text/css">
+                        ' . $cssContent . '
+                    </style>
+                ', $message);
+            }
+        }
+        return $message;
+    }
+
+    /**
+     * @param $linkElement
+     * @return string
+     */
+    private function getCssContent($linkElement)
+    {
+        // load data from html element
+        preg_match_all("/href=['\"]([^'\"]+)['\"]/", $linkElement, $matches);
+        $url = $matches[1][0];
+
+        $absUrl = $url;
+        
+        // this needs to be changed to search in all include paths
+        $cssContent = @file_get_contents($absUrl);
+
+        if (!$cssContent) {
+            trigger_error('Error loading stylesheet from ' . $absUrl, E_USER_NOTICE);
+        }
+
+        return $cssContent;
+    }
+
 
     /**
      * Do nothing
